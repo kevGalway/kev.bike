c97549e0bbeac757eff4e9dac1acce78f7477f08
Kevin <kev9229@gmail.com>
2025-04-19 09:42:56 -0500

    remove bad gps cords and update script to check for bad data.

--- update.sh
+++ update.sh
@@ -1,13 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 cd ~/repos/kev.bike
-location=$(termux-location | jq '.')
+location=$(termux-location 2>/dev/null | jq '.')
+
 latitude=$(echo "$location" | jq '.latitude')
 longitude=$(echo "$location" | jq '.longitude')
 
+# Check if latitude and longitude are numbers
+if ! [[ "$latitude" =~ ^-?[0-9.]+$ ]] || ! [[ "$longitude" =~ ^-?[0-9.]+$ ]]; then
+  echo "Error: Invalid GPS coordinates. Skipping update."
+  sleep 5
+  exit 1
+fi
+
 FILE="map/index.html"
 
-sed -i "s/];/  { lat: $latitude, lng: $longitude },\n];/" map/index.html
+sed -i "s/];/  { lat: $latitude, lng: $longitude },\n];/" "$FILE"
 
 # Commit and push the changes to Git
 git add "$FILE"
@@ -15,6 +23,3 @@ git commit -m "Added new coordinates to routeCoordinates: lat=$latitude, lng=$lo
 git push origin main
 
 echo "Coordinates added, changes committed, and pushed successfully."
-
-
-
